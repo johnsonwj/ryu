dc8203c881fd5d8c595f7e75d7a512d60f8bd586
Will Johnson <william.johnson@accolade.com>
2019-12-22 09:26:31 -0800

    simple works

--- src/Text/Format/Floating/IEEE.hs
+++ src/Text/Format/Floating/IEEE.hs
@@ -1,38 +1,62 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving, ScopedTypeVariables #-}
 
 module Text.Format.Floating.IEEE
-    ( ieeeDecodeFloat
-    , ieeeFloatBias
-    , IEEEFloat, ieeeSign, ieeeExponent, ieeeMantissa
-    , Sign, signValue, floatSign
-    , Exponent, exponentValue, floatExponent
-    , Mantissa, mantissaValue, floatMantissa
+    ( IEEEFloat(..)
+    , IEEEComponents(..)
+    , Sign, signValue
+    , Exponent, exponentValue
+    , Mantissa, mantissaValue
     , showBits
     ) where
 
 import Data.Bits
+import GHC.Float (castFloatToWord32, castDoubleToWord64)
 
-{-|
-    Haskell's 'decodeFloat' function is somewhat inconvenient for our purposes. Ryu defines its
-    variables like this:
-        normalizedFloat = (-1)^s * (1.m) * 2^(e - bias)
-        denormalizedFloat = (-1)^s * (0.m) * 2^(1 - bias) [exponent bits are all 0]
-        
--}
+class RealFloat a => IEEEFloat a where
+    ieeeDecodeFloat :: a -> IEEEComponents a
 
-ieeeDecodeFloat :: (RealFloat a) => a -> Maybe (IEEEFloat a)
-ieeeDecodeFloat x
-    | isIEEE x  = Just (ieeeDecodeFloatUnsafe x)
-    | otherwise = Nothing
+    -- | These methods do not evaluate their argument.
+    ieeeFloatBias :: a -> Integer
+    ieeeExponentLength :: a -> Integer
+    ieeeMantissaLength :: a -> Integer
 
-ieeeDecodeFloatUnsafe :: (RealFloat a) => a -> IEEEFloat a
-ieeeDecodeFloatUnsafe x = IEEEFloat (floatSign x) (floatExponent x) (floatMantissa x)
+float32Bias, float32ExponentLength, float32MantissaLength:: Integer
+float32Bias = 127
+float32ExponentLength = 8
+float32MantissaLength = 23
 
--- | This function does not evaluate its argument.
-ieeeFloatBias :: forall a. (RealFloat a) => IEEEFloat a -> Int
-ieeeFloatBias _ = let biasedExpMax = snd $ floatRange (undefined :: a) in biasedExpMax - 1
+instance IEEEFloat Float where
+    ieeeDecodeFloat x = unsafeDecodeFloat (toInteger $ castFloatToWord32 x) float32ExponentLength float32MantissaLength
+    ieeeFloatBias = const float32Bias
+    ieeeExponentLength = const float32ExponentLength
+    ieeeMantissaLength = const float32MantissaLength
 
-data IEEEFloat a = IEEEFloat
+float64Bias, float64ExponentLength, float64MantissaLength :: Integer
+float64Bias = 1023
+float64ExponentLength = 11
+float64MantissaLength = 52
+
+instance IEEEFloat Double where
+    ieeeDecodeFloat x = unsafeDecodeFloat (toInteger $ castDoubleToWord64 x) float64ExponentLength float64MantissaLength
+    ieeeFloatBias = const float64Bias
+    ieeeExponentLength = const float64ExponentLength
+    ieeeMantissaLength = const float64MantissaLength
+
+unsafeDecodeFloat :: Integer -> Integer -> Integer -> IEEEComponents a
+unsafeDecodeFloat bits es ms = IEEEComponents (Sign s) (Exponent e) (Mantissa m) where
+    ms' = fromInteger ms
+    es' = fromInteger es
+    m = maskRange 0 ms' .&. bits
+    e = rotateR (maskRange ms' es' .&. bits) ms'
+    s = testBit bits (ms' + es')
+
+-- | @mantissaMask l s@ is an 'Integer' mask for the @s@ consecutive bits starting
+--   at index @l@. For example: @mantissaMask 4 6 == 0b0001111110000@.
+maskRange :: Int -> Int -> Integer
+maskRange _ 0 = zeroBits
+maskRange l s = bit (l + s - 1) .|. maskRange l (s - 1)
+
+data IEEEComponents a = IEEEComponents
     { ieeeSign      :: Sign a
     , ieeeExponent  :: Exponent a
     , ieeeMantissa  :: Mantissa a 
@@ -41,9 +65,6 @@ data IEEEFloat a = IEEEFloat
 -- | This type carries a 'Bool' representing the sign bit of an IEEE floating-point number.
 --   @Sign False@ indicates a positive number; @Sign True@ indicates a negative.
 newtype Sign a = Sign { signValue :: Bool } deriving (Eq, Bits, FiniteBits, Show)
-
-floatSign :: (RealFloat a) => a -> Sign a
-floatSign x = Sign (signum x < 0)
 
 -- | This type carries an arbitrary-precision integer representing the exponent of an IEEE
 --   floating-point number, interpreted directly as an integer. For example:
@@ -60,41 +81,10 @@ floatSign x = Sign (signum x < 0)
 --   no way to determine the actual number of "significant" (sorry) bits in the value.
 newtype Exponent a = Exponent { exponentValue :: Integer } deriving (Eq, Bits, Show)
 
-instance RealFloat a => FiniteBits (Exponent a) where
+instance IEEEFloat a => FiniteBits (Exponent a) where
 
     -- | This implementation does not evaluate its argument.
-    finiteBitSize _ = nbitsExp where
-        (expMin, expMax) = floatRange (undefined :: a)
-
-        -- +2 is because Haskell 'floatRange' does not include the min/max /encodeable/
-        -- exponents (all zeroes or all ones, respectively): only the range of exponents
-        -- for a normalized float value. For instance, a 32-bit float has 8 bits in the
-        -- exponent, for a maximum unbiased exponent of @0b11111111 :: Int = 255@.
-        -- Meanwhile, @floatRange (undefined :: Float) = (-125, 128)@ has a difference
-        -- of 253, because it does not include the (biased) lowest value -126 or the
-        -- (biased) highest value of 129.
-        unbiasedMax = (expMax - expMin) + 2
-
-        -- 'floatRange' returns a pair of 'Int's, which is a fixed-precision type
-        -- (the actual size of the 'Int' type doesn't matter here). This enables
-        -- a bit of a shortcut: @unbiasedMax@ will look like @0b0..01..1@
-        -- where the number of ones is the same as the size of the exponent
-        nbitsExp = finiteBitSize unbiasedMax - countLeadingZeros unbiasedMax
-
-floatExponent :: forall a. RealFloat a => a -> Exponent a
-floatExponent x = Exponent (toInteger unscaledExp) where
-    unscaledExp = scaledBiasedExponent + bias - scale
-
-    -- Haskell's 'exponent' returns the biased exponent, scaled so that 'significand' will
-    -- return a number in the open interval (-1, 1). For denormalized numbers, this is
-    -- equivalent to the biased exponent, because the significand is already less than one
-    -- in absolute value. But for normalized numbers, the implicit leading bit means that
-    -- the significand will be divided by two, increasing the exponent by one. So we have
-    -- to undo that here.
-    scaledBiasedExponent = exponent x
-    bias = ieeeFloatBias (undefined :: IEEEFloat a)
-    scale = if isDenormalized x then 0 else 1
-
+    finiteBitSize _ = fromInteger $ ieeeExponentLength (undefined :: a)
 
 -- | This type carries an arbitrary-precision integer representing the mantissa of an IEEE
 --   floating-point number, interpreted directly as an integer. For example:
@@ -111,23 +101,13 @@ floatExponent x = Exponent (toInteger unscaledExp) where
 --   no way to determine the actual number of "significant" (sorry) bits in the value.
 newtype Mantissa a = Mantissa { mantissaValue :: Integer } deriving (Eq, Bits, Show)
 
-instance RealFloat a => FiniteBits (Mantissa a) where
+instance IEEEFloat a => FiniteBits (Mantissa a) where
 
     -- | This is the number of actual encoded bits in the mantissa of this float, /not/ including the
     --   implicit bit (which Haskell's floatDigits does include).
     --
     --   This implementation does not evaluate its argument.
-    finiteBitSize _ = floatDigits (undefined :: a) - 1
-
-floatMantissa :: forall a. RealFloat a => a -> Mantissa a
-floatMantissa x = Mantissa rescaledMantissa where
-    (scaledSignificand, scaledExponent) = decodeFloat x
-
-    mantissaDigits = finiteBitSize (undefined :: Mantissa a)
-
-    rescaledSignificand = scaledSignificand * (floatRadix x ^ (scaledExponent + mantissaDigits - 1))
-
-    rescaledMantissa = clearBit rescaledSignificand mantissaDigits
+    finiteBitSize _ = fromInteger $ ieeeMantissaLength (undefined :: a)
 
 showBits :: (FiniteBits b) => b -> String
 showBits b = reverse (buildBitsString 0) where
--- src/Text/Format/Floating/Simple.hs
+++ src/Text/Format/Floating/Simple.hs
@@ -3,29 +3,123 @@
     without using lookup tables to speed up the loops. 
 -}
 
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 
-module Text.Format.Floating.Simple (formatDec, formatSci) where
+module Text.Format.Floating.Simple where -- (format, formatDec, formatSci, formatHex) where
+
+import Text.Format.Floating.IEEE
 
 import Data.Text (Text)
-import Data.Binary (Binary, Word8, encode)
-import Data.ByteString.Lazy (unpack)
-import Data.Bits
+import qualified Data.Text as T
+import Data.Char (intToDigit)
+import Data.Bits (popCount, finiteBitSize)
 
-formatDec :: (RealFloat a) => a -> Text
-formatDec = undefined
+formatDec :: (IEEEFloat a) => a -> Text
+formatDec = formatDecimal renderDec
 
-formatSci :: (RealFloat a) => a -> Text
-formatSci = undefined
+formatSci :: (IEEEFloat a) => a -> Text
+formatSci = formatDecimal renderSci
+
+formatHex :: (IEEEFloat a) => a -> Text
+formatHex x = undefined where
+    IEEEComponents cs ce cm = ieeeDecodeFloat x
+    m = mantissaValue cm
+    e = exponentValue ce
+
+type Renderer = Bool -> Integer -> Integer -> Text
+
+formatDecimal :: (IEEEFloat a) => Renderer -> a -> Text
+formatDecimal render x = case convertToDecimal x of
+    NaN     -> "NaN"
+    PosInf  -> "Infinity"
+    NegInf  -> "-Infinity"
+    DecimalFloat s d e  -> render s d e
+
+renderDec :: Renderer
+renderDec s d e
+    | e == 0    = T.append (renderIntegerDecimal s d) ".0"
+    | e > 0     = T.concat [renderIntegerDecimal s d, T.replicate (fromInteger e) "0", ".0"]
+    | otherwise = let
+        dr = renderIntegerDecimal s d
+        dl = T.length dr
+        dd = dl + fromInteger e
+        in if dd <= 0
+            then T.concat ["0.", T.replicate (abs dd) "0", dr]
+            else let (w, f) = T.splitAt dd dr in T.concat [w, ".", f]
+
+renderSci :: Renderer
+renderSci s d e = T.concat [dh', ".", dt', "e", es, renderIntegerDecimal False $ abs e'] where
+    (dh, dt) = T.splitAt 1 (renderIntegerDecimal False d)
+    dh' = if s then T.cons '-' dh else dh
+    dt' = if T.null dt then "0" else dt
+    e' =  e + fromIntegral (T.length dt)
+    es = if e' < 0 then "-" else "+"
+
+data DecimalFloat = NaN | PosInf | NegInf | DecimalFloat Bool Integer Integer deriving (Eq, Show)
 
 -- | Convert a given float to the form (m10, e10) where m10 is the smallest valid integer,
 --   such that a = m10 * 10 ^ e10.
-convertToDecimal :: (RealFloat a) => a -> (Integer, Int)
-convertToDecimal a
-    | isInfinite a                  = undefined --  if signum a < 0 then ('-' :)
-    | floatRadix a == 10            = decodeFloat a
-    | floatRadix a == 2 && isIEEE a = uncurry ieeeBinToDecimal (decodeFloat a)
-    | otherwise                     = error "we only support IEEE floats with radix 2"
+convertToDecimal :: (IEEEFloat a) => a -> DecimalFloat
+convertToDecimal x
+    | popCount ce == finiteBitSize ce   = case (s, m) of
+        (False, 0)  -> PosInf
+        (True, 0)   -> NegInf
+        (_, _)      -> NaN
+    | otherwise = let
+        mf = if e == 0 then m else m + 2 ^ ml
+        ef = (if e == 0 then 1 else e) - bias - ml
 
-ieeeBinToDecimal :: Integer -> Int -> (Integer, Int)
-ieeeBinToDecimal m2 e2 = undefined
+        e2 = ef - 2
+
+        uvw = addIntervals (intervalOf $ 4 * mf) (FloatInterval (if m == 0 && e > 1 then -1 else -2, 2))
+
+        (e10, FloatInterval (a, c)) = if e2 >= 0
+            then (0, scaleInterval uvw (2 ^ e2))
+            else (e2, scaleInterval uvw (5 ^ abs e2))
+
+        
+        (d0, e0) = shortestDecimalRep a c True True
+        in DecimalFloat s d0 (e0 + e10)
+    where
+        IEEEComponents cs ce cm = ieeeDecodeFloat x
+
+        s = signValue cs
+        m = mantissaValue cm
+        e = exponentValue ce
+
+        ml = ieeeMantissaLength x
+        bias = ieeeFloatBias x
+
+newtype FloatInterval = FloatInterval (Integer, Integer)
+
+scaleInterval :: FloatInterval -> Integer -> FloatInterval
+scaleInterval (FloatInterval (a, c)) s = FloatInterval (a * s, c * s)
+
+addIntervals :: FloatInterval -> FloatInterval -> FloatInterval
+addIntervals (FloatInterval (a, c)) (FloatInterval (a', c')) = FloatInterval (a + a', c + c')
+
+intervalOf :: Integer -> FloatInterval
+intervalOf x = FloatInterval (x, x)
+
+shortestDecimalRep :: Integer -> Integer -> Bool -> Bool -> (Integer, Integer)
+shortestDecimalRep a0 c0 acceptSmaller acceptLarger = go True a0 (if acceptLarger then c0 else c0 - 1) 0 where
+    go az a c i = let
+        a' = a `div` 10
+        c' = c `div` 10
+        adz = a `mod` 10 == 0
+        az' = az && adz
+        in if  a' < c' || (acceptSmaller && az')
+            then go az' a' c' (i + 1)
+            else (c, i)
+
+
+renderIntegerDecimal :: Bool -> Integer -> Text
+renderIntegerDecimal False 0 = "0"
+renderIntegerDecimal True 0 = "-0"
+renderIntegerDecimal sgn x = let
+    addSign = if sgn then T.cons '-' else id
+    (q, r) = x `quotRem` 10
+    rc = intToDigit $ fromIntegral r
+    in addSign $ if q > 0
+        then T.snoc (renderIntegerDecimal False q) rc
+        else T.singleton rc
